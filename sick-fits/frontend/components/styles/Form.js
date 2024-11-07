4cb5ca0b5ab4354bce23dc5b0ae9c1a907aa3d9d
Adam Antkowiak <adam.antkowiak93@gmail.com>
2024-11-07 17:21:07 +0100

    Lesson 35 state

--- sick-fits/frontend/components/styles/Form.js
+++ sick-fits/frontend/components/styles/Form.js
@@ -45,6 +45,10 @@ const Form = styled.form`
     font-size: 2rem;
     font-weight: 600;
     padding: 0.5rem 1.2rem;
+    cursor: pointer;
+    :hover {
+      background-color: orange;
+    }
   }
   fieldset {
     border: 0;
