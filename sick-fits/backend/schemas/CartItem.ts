e25027ac774218f8319203dcda4995ddaeb3a2f8
Adam Antkowiak <adam.antkowiak93@gmail.com>
2024-11-13 15:00:50 +0100

    Lesson 70 state

--- sick-fits/backend/schemas/CartItem.ts
+++ sick-fits/backend/schemas/CartItem.ts
@@ -1,14 +1,25 @@
+import { integer, select, text, relationship } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
-import { text, password, relationship, integer } from '@keystone-next/fields';
+import { rules, isSignedIn } from '../access';
 
 export const CartItem = list({
+  access: {
+    create: isSignedIn,
+    read: rules.canOrder,
+    update: rules.canOrder,
+    delete: rules.canOrder,
+  },
   ui: {
     listView: {
       initialColumns: ['product', 'quantity', 'user'],
     },
   },
   fields: {
-    quantity: integer({ defaultValue: 1, isRequired: true }),
+    // TODO: Custom Label in here
+    quantity: integer({
+      defaultValue: 1,
+      isRequired: true,
+    }),
     product: relationship({ ref: 'Product' }),
     user: relationship({ ref: 'User.cart' }),
   },
